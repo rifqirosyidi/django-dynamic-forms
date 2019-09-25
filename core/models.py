f7e5cfc9edc564d50a88660a1c735b20d350ad31
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-25 19:27:22 +0700

    creating models

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Journal(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    reviewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
