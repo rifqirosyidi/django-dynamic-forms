02f6b0ca04f26df64d4b123442a7d7b898d1821a
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-26 14:57:14 +0700

    search filtering

--- core/views.py
+++ core/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from core.models import Journal
+from django.db.models import Q
 
 
 # Create your views here.
 def bootstrap_filter_view(request):
-    return render(request, "bootstrap_form.html")
+    qs = Journal.objects.all()
+
+    title_contains = request.GET.get('title_contains')
+    id_exact = request.GET.get('id_exact')
+    title_or_author = request.GET.get('title_or_author')
+
+    if title_contains != '' and title_contains is not None:
+        qs = qs.filter(title__icontains=title_contains)
+
+    if id_exact != '' and id_exact is not None:
+        qs = qs.filter(id=id_exact)
+
+    if title_or_author != '' and title_or_author is not None:
+        qs = qs.filter(
+            Q(title__icontains=title_or_author) |
+            Q(author__name__icontains=title_or_author)
+        ).distinct()
+
+    context = {
+        'query_set': qs
+    }
+
+    return render(request, "bootstrap_form.html", context)
