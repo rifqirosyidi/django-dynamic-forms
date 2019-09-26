4eb878e2c53db7e410d77185a8e0e30423094136
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-26 15:40:32 +0700

    filtering by views and datetime

--- core/views.py
+++ core/views.py
@@ -3,6 +3,10 @@ from core.models import Journal
 from django.db.models import Q
 
 
+def is_query_params_valid(params):
+    return params != '' and params is not None
+
+
 # Create your views here.
 def bootstrap_filter_view(request):
     qs = Journal.objects.all()
@@ -10,18 +14,36 @@ def bootstrap_filter_view(request):
     title_contains = request.GET.get('title_contains')
     id_exact = request.GET.get('id_exact')
     title_or_author = request.GET.get('title_or_author')
+    view_count_min = request.GET.get('view_count_min')
+    view_count_max = request.GET.get('view_count_max')
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
 
-    if title_contains != '' and title_contains is not None:
+    if is_query_params_valid(title_contains):
         qs = qs.filter(title__icontains=title_contains)
 
-    if id_exact != '' and id_exact is not None:
+    if is_query_params_valid(id_exact):
         qs = qs.filter(id=id_exact)
 
-    if title_or_author != '' and title_or_author is not None:
+    if is_query_params_valid(title_or_author):
         qs = qs.filter(
             Q(title__icontains=title_or_author) |
             Q(author__name__icontains=title_or_author)
         ).distinct()
+
+    # Views Check
+    if is_query_params_valid(view_count_min):
+        qs = qs.filter(views__gte=view_count_min)
+
+    if is_query_params_valid(view_count_max):
+        qs = qs.filter(views__lte=view_count_max)
+
+    # Publish Date Check
+    if is_query_params_valid(date_min):
+        qs = qs.filter(publish_date__gte=date_min)
+
+    if is_query_params_valid(date_max):
+        qs = qs.filter(publish_date__lte=date_max)
 
     context = {
         'query_set': qs
